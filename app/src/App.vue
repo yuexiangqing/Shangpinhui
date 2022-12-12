567a74ec2d635accba232153088a92cfa557d599
yuexiaogou <xiangqing0914@icloud.com>
2022-12-12 14:07:01 +0800

    Header 和 Footer 非路由组件的完成

---
+++ app/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Header></Header>
+    <Footer></Footer>
+    我是根组件
+    </div>
+</template>
+
+<script>
+import Header from './components/Header'
+import Footer from './components/Footer'
+export default {
+name: 'AppIndex',
+components:{
+  Header,
+  Footer
+}
+}
+</script>
+
+<style>
+
+</style>>
